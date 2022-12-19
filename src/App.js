fbf6df0a63e3920d61bbf9aa5523c64d84f739c0
Shoaib Khan <shoaibpathan812@gmail.com>
2022-12-19 17:31:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React , {useState} from 'react'
+import axios from 'axios'  
+
+
+
+const App = () => {
+  const [photo, setPhoto] = useState("")
+  const [result, setResult] = useState([])
+
+
+const changePhoto = () => {
+   axios.get( `https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=uV67R6UbUZ4FzUZWjwi89RqN65LSiro291dSqR3A8cY `)
+   .then((response)=>{
+    console.log(response.data );
+    setResult(response.data.results);
+   })
+}
+
+
+  return (
+    <>
+    <div className='container text-center my-5'>
+       <input type="text" className='form-control ' value={photo} onChange={(e) => {
+                    setPhoto(e.target.value)
+                }} />
+      <button type='submit' onClick={changePhoto} className='btn btn-primary my-2'>Get Photo</button>
+      
+    </div>
+     
+
+     
+    <div className="container">
+                <div class="row text-center text-lg-start">
+                    {result.map((value) => {
+                        return (
+                            <div class="col-lg-3 col-md-4 col-6">
+                                    <img class="img-fluid img-thumbnail d-block mb-4 h-100" src={value.urls.regular} alt='' />
+                            </div>
+                        )
+                    })}
+                </div>
+
+            </div>
+       
+  
+
+
+    
+  </>
+
+  )
+}            
+
+export default App
